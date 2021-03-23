32140be5ca232d96a010c32fb2656acb424ea3ca
van_yog <ikharchenko2008@gmail.com>
2021-03-23 16:35:33 +0200

    start project

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React, {Component} from "react";
+import FilmsList from "pages/FilmsPage/components/FilmsList";
+import {films} from "data";
+
+class App extends Component {
+  state = {
+    films: [],
+  };
+
+  componentDidMount() {
+    this.setState({films});
+  }
+
+  onReset = () => this.setState({films});
+
+  render() {
+    const {films} = this.state;
+    return (
+      <div className="ui container mt-3">
+        <FilmsList films={films} />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/test.js
@@ -0,0 +1,12 @@
+const name = "Bill";
+typeof name === "strng";
+
+if (!"serviceWorker" in navigator) {
+  // you have an old browser
+}
+
+const greeting = "Hello";
+console
+  .log("${greeting} world")
+
+  [(1, 2, 3)].forEach(x => console.log(x));
